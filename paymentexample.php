d45336cdffa1ee1f08cdf47599a74e35822d5ea4
Maxime Biloé <maxime.biloe@prestashop.com>
2015-11-09 14:33:03 +0100

    //Initial commit

---
+++ paymentexample.php
@@ -0,0 +1,189 @@
+<?php
+/*
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2015 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class PaymentExample extends PaymentModule
+{
+    protected $_html = '';
+    protected $_postErrors = array();
+
+    public $details;
+    public $owner;
+    public $address;
+    public $extra_mail_vars;
+
+    public function __construct()
+    {
+        $this->name = 'paymentexample';
+        $this->tab = 'payments_gateways';
+        $this->version = '1.0.0';
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->author = 'PrestaShop';
+        $this->controllers = array('validation');
+        $this->is_eu_compatible = 1;
+
+        $this->currencies = true;
+        $this->currencies_mode = 'checkbox';
+
+        $this->bootstrap = true;
+        parent::__construct();
+
+        $this->displayName = $this->l('Payment Example');
+        $this->description = $this->l('Description of Payment Example');
+
+        if (!count(Currency::checkPaymentCurrencies($this->id))) {
+            $this->warning = $this->l('No currency has been set for this module.');
+        }
+    }
+
+    public function install()
+    {
+        if (!parent::install() || !$this->registerHook('payment')  || !$this->registerHook('paymentOptions') || !$this->registerHook('paymentReturn')) {
+            return false;
+        }
+        return true;
+    }
+
+    public function hookPaymentOptions($params)
+    {
+        if (!$this->active) {
+            return;
+        }
+
+        if (!$this->checkCurrency($params['cart'])) {
+            return;
+        }
+
+        $payment_options = [
+            $this->getOfflinePaymentOption(),
+            $this->getExternalPaymentOption(),
+            $this->getEmbeddedPaymentOption(),
+            $this->getIframePaymentOption(),
+        ];
+
+        return $payment_options;
+    }
+
+    public function checkCurrency($cart)
+    {
+        $currency_order = new Currency($cart->id_currency);
+        $currencies_module = $this->getCurrency($cart->id_currency);
+
+        if (is_array($currencies_module)) {
+            foreach ($currencies_module as $currency_module) {
+                if ($currency_order->id == $currency_module['id_currency']) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function getOfflinePaymentOption()
+    {
+        $offlineOption = new Core_Business_Payment_PaymentOption();
+        $offlineOption->setCallToActionText($this->l('Pay offline'))
+                      ->setAction($this->context->link->getModuleLink($this->name, 'validation', array(), true))
+                      ->setAdditionalInformation($this->context->smarty->fetch(implode(DIRECTORY_SEPARATOR, [__DIR__, 'views', 'templates', 'front', 'payment_infos.tpl'])))
+                      ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/payment.jpg'));
+
+        return $offlineOption;
+    }
+
+    public function getExternalPaymentOption()
+    {
+        $externalOption = new Core_Business_Payment_PaymentOption();
+        $externalOption->setCallToActionText($this->l('Pay external'))
+                       ->setAction($this->context->link->getModuleLink($this->name, 'validation', array(), true))
+                       ->setMethod('POST')
+                       ->setInputs([
+                            'token' => [
+                                'name' =>'token',
+                                'type' =>'hidden',
+                                'value' =>'12345689',
+                            ],
+                        ])
+                       ->setAdditionalInformation($this->context->smarty->fetch(implode(DIRECTORY_SEPARATOR, [__DIR__, 'views', 'templates', 'front', 'payment_infos.tpl'])))
+                       ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/payment.jpg'));
+
+        return $externalOption;
+    }
+
+    public function getEmbeddedPaymentOption()
+    {
+        $embeddedOption = new Core_Business_Payment_PaymentOption();
+        $embeddedOption->setCallToActionText($this->l('Pay embedded'))
+                       ->setForm($this->generateForm())
+                       ->setAdditionalInformation($this->context->smarty->fetch(implode(DIRECTORY_SEPARATOR, [__DIR__, 'views', 'templates', 'front', 'payment_infos.tpl'])))
+                       ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/payment.jpg'));
+
+        return $embeddedOption;
+    }
+
+    public function getIframePaymentOption()
+    {
+        $iframeOption = new Core_Business_Payment_PaymentOption();
+        $iframeOption->setCallToActionText($this->l('Pay iframe'))
+                     ->setAction($this->context->link->getModuleLink($this->name, 'iframe', array(), true))
+                     ->setAdditionalInformation($this->context->smarty->fetch(implode(DIRECTORY_SEPARATOR, [__DIR__, 'views', 'templates', 'front', 'payment_infos.tpl'])))
+                     ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/payment.jpg'));
+
+        return $iframeOption;
+    }
+
+    protected function generateForm()
+    {
+        $months = [];
+        for ($i = 1; $i <= 12; $i++) {
+            $months[] = sprintf("%02d", $i);
+        }
+
+        $years = [];
+        for ($i = 0; $i <= 10; $i++) {
+            $years[] = date('Y', strtotime('+'.$i.' years'));
+        }
+
+        $this->context->smarty->assign([
+            'action' => $this->context->link->getModuleLink($this->name, 'validation', array(), true),
+            'months' => $months,
+            'years' => $years,
+        ]);
+
+        return $this->context->smarty->fetch(
+            implode(
+                DIRECTORY_SEPARATOR,
+                [__DIR__,
+                'views',
+                'templates',
+                'front',
+                'payment_form.tpl']
+            )
+        );
+    }
+}
---
+++ views/templates/front/iframe.tpl
@@ -0,0 +1,7 @@
+{extends "$layout"}
+
+{block name="content"}
+  <section>
+    <iframe src="{$src}" width="900" height="400" ></iframe>
+  </section>
+{/block}
